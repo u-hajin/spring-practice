ca34d04fa835abeb9f9b8eecf6540d26f80d01bd
YooSuYeon <usuyn000@gmail.com>
2022-07-30 23:32:02 +0900

    add: mvc, template engine

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model) { // spring이 model을 만들어 넣어줌
         model.addAttribute("data", "hello!!");
         return "hello"; // templates/hello.html 화면을 실행
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
