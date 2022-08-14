2430bd261fe12884cf63e5c7fa13c4210548206a
YooSuYeon <usuyn000@gmail.com>
2022-08-14 23:53:11 +0900

    feat: member sign in

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // 스프링 컨테이너에 Controller 객체를 생성해 넣어둔다. 스프링 빈이 관리된다.
 @Controller
@@ -18,5 +21,20 @@ public class MemberController {
 //    public void setMemberService(MemberService memberService) {
 //        this.memberService = memberService;
 //    }
-    
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new") // data를 form에 넣어서 전달할 때 post, 데이터 등록
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // 회원가입이 끝나면 홈 화면으로
+    }
+
 }
