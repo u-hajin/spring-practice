015bbc231727deaa4bedd6871539550a534f7cf3
YooSuYeon <usuyn000@gmail.com>
2022-08-25 23:56:38 +0900

    add: JPA

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 고객이 지정하는 ID X, system이 저장하는 ID
     private String name;
 
